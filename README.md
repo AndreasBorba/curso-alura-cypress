bec18be8f83878f2351566fd46ba18b595c4f464
AndreasBorba <borba_andreas@hotmail.com>
2022-05-24 12:30:21 -0300

    Remoção de arquivos e inclusão de novos Casos de testes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Comandos utilizados:
 - npm init
 - npm install cypress --save-dev
+- npm install mochawesome --save-dev ou npm -i -D mochawesome
--- cypress/integration/3-alurapic/alurapic.spec.js
+++ cypress/integration/3-alurapic/alurapic.spec.js
@@ -11,6 +11,28 @@ describe('Login e registro de usuario Alurapic', () => {
         cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+    })
 
+    it('verifica mensagem de e-mail inválido', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('Andreas');
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+    })
+
+    it('verifica mensagem de senha com menos de 8 caracteres', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+    })
+
+    it('verifica mensagem de nome com caractere minúsculo', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="userName"]').type('T');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
     })
 })
